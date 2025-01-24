ac97e606fab7fd63805aa2111f37d96b3d4b4c1c
Omole kessiena David <edokadavid@gmail.com>
2025-01-24 08:39:53 +0100

    update

--- app/Http/Controllers/Api/v1/Auth/AuthController.php
+++ app/Http/Controllers/Api/v1/Auth/AuthController.php
@@ -31,7 +31,6 @@ class AuthController extends Controller
         $validatedData['status'] = Status::ACTIVE;
         $validatedData['sv'] = Status::ACTIVE;
 
-
         $user = User::create($validatedData);
 
 
