b67c062611fa9ca4a8c3c8bffdf8624693c3a678
Omole kessiena David <edokadavid@gmail.com>
2025-02-25 13:06:24 +0100

    update

--- app/Http/Resources/v1/GigResource.php
+++ app/Http/Resources/v1/GigResource.php
@@ -14,27 +14,28 @@ class GigResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-         return [
-        'type' => 'Gig',
-        'id' => $this->id,
-        'attributes' => [
-            'title' => $this->title,
-            'description' => $this->description,
-            'skills' => $this->skills, // Assuming it's stored as an array (JSON column)
-            'location' => $this->location,
-            'image' => $this->image,
-            'previous_works_companies' => $this->previous_works_companies, // Array
-            'language' => $this->language,
-            'unique_selling_point' => $this->unique_selling_point,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-        ],
-        'relationships' => [
-            'plans' => json_decode($this->plans, true),
-            'reviews' => ReviewResource::collection($this->whenLoaded('reviews')),
+        return [
+            'type' => 'Gig',
+            'id' => $this->id,
+            'attributes' => [
+                'title' => $this->title,
+                'description' => $this->description,
+                'skills' => $this->skills, // Assuming it's stored as an array (JSON column)
+                'location' => $this->location,
+                'image' => $this->image,
+                'previous_works_companies' => $this->previous_works_companies, // Array
+                'language' => $this->language,
+                'unique_selling_point' => $this->unique_selling_point,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ],
+            'creator' => new UserResource($this->whenLoaded('user')),
+            'relationships' => [
+                'plans' => json_decode($this->plans, true),
+                'reviews' => ReviewResource::collection($this->whenLoaded('reviews')),
 //            'created_by' => new UserResource($this->user), // Assuming a `creator` relation exists
 //            'plans' => GigPlanResource::collection($this->plans), // Assuming a `plans` relation
-        ]
-    ];
+            ]
+        ];
     }
 }
