cfe00902e51ef3506bfcd9b2bc97712262f82cee
Omole kessiena David <edokadavid@gmail.com>
2025-05-08 12:57:03 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -52,8 +52,8 @@ Route::get('/check-auth', function() {
     }
 })->middleware('auth:sanctum');
 
-Route::post('/send-message', [MessageController::class, 'sendMessage'])->name('send.message');
-Route::get('/conversation/{conversationId}/messages', [MessageController::class, 'getMessages']);
+Route::post('/send-message', [MessageController::class, 'sendMessage'])->name('send.message')->middleware('auth:sanctum');
+Route::get('/conversation/{conversationId}/messages', [MessageController::class, 'getMessages'])->middleware();
 Route::get('/pusher-credentials', function () {
     return response()->json([
         'PUSHER_APP_KEY' => env('PUSHER_APP_KEY'),
