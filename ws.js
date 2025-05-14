bda8dab4afe06e9ec8c49a8b5603600c02f2372a
Omole kessiena David <edokadavid@gmail.com>
2025-05-14 18:43:57 +0100

    update ws.js

--- ws.js
+++ ws.js
@@ -1,7 +1,27 @@
-const { spawn } = require('child_process');
-const server = spawn('php', ['artisan', 'reverb:start'], {
-  detached: true,
-  stdio: 'ignore'
+const net = require('net');
+
+const port = 6001; // or 6005
+
+const server = net.createServer();
+
+server.once('error', function (err) {
+    if (err.code === 'EADDRINUSE') {
+        console.log(`Port ${port} is already in use. Reverb is likely running.`);
+    } else {
+        console.error(err);
+    }
 });
-server.unref();
-process.exit(0);
+
+server.once('listening', function () {
+    server.close();
+
+    const { spawn } = require('child_process');
+    const reverb = spawn('php', ['artisan', 'reverb:start'], {
+        detached: true,
+        stdio: 'ignore'
+    });
+    reverb.unref();
+    console.log(`Reverb started on port ${port}`);
+});
+
+server.listen(port);
