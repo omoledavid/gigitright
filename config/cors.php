519cc18c8ce0c7a1910d343854356f29d605dd44
Omole kessiena David <edokadavid@gmail.com>
2025-05-15 07:26:39 +0100

    update cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'https://api.gigitright.com'],
+    'allowed_origins' => ['https://gigitright.com', 'https://api.gigitright.com'],
 
     'allowed_origins_patterns' => [],
 
