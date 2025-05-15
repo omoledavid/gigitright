466f4f39de4dfda188f40a54734816b98dfa3000
Omole kessiena David <edokadavid@gmail.com>
2025-05-15 12:46:37 +0100

    fixes

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s%s',
-        'localhost,http://localhost:8080,localhost:8080,localhost:3000,127.0.0.1,127.0.0.1:3000,127.0.0.1:8000,::1',
+        'localhost,http://localhost:8080,localhost:8080,localhost:3000,127.0.0.1,127.0.0.1:3000,127.0.0.1:8000,localhost:5173,::1',
         Sanctum::currentApplicationUrlWithPort(),
         env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
     ))),
