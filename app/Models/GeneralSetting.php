bdfdab5f13535bdf5be425b7208fda2a487f06d7
Omole kessiena David <edokadavid@gmail.com>
2025-06-13 09:46:24 +0100

    update

--- app/Models/GeneralSetting.php
+++ app/Models/GeneralSetting.php
@@ -56,7 +56,7 @@ class GeneralSetting extends Model
         'en' => 'boolean',
         'maintenance_mode' => 'boolean',
         'login_status' => 'boolean',
-        'gft_rate' => 'integer',
+        'gft_rate' => 'decimal',
     ];
     // protected $hidden = ['email_template','mail_config','sms_config','system_info'];
 
